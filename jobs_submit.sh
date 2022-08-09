d67b182c899f3a2833a00c86acad60c8cfcb93ca
Felix Pichardo <fpichard@umn.edu>
2022-08-09 10:40:24 -0500

    Upddate data for testing 2

--- jobs_submit.sh
+++ jobs_submit.sh
@@ -26,10 +26,10 @@ esac
 
 echo Submitting job...
 echo sbatch --job-name=cv_alg-${1}_outcome-ar_stdprice_total_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_outcome-ar_stdprice_total.sbatch
-#sbatch --job-name=cv_alg-${1}_outcome-ar_stdprice_total_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_outcome-ar_stdprice_total.sbatch
+sbatch --job-name=cv_alg-${1}_outcome-ar_stdprice_total_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_outcome-ar_stdprice_total.sbatch
 echo 
 
 echo Submitting job...
 echo sbatch --job-name=cv_alg-${1}_outcome-ar_stdprice_total_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_outcome-ar_stdprice_total.sbatch
-#sbatch --job-name=cv_alg-${1}_outcome-ar_stdprice_total_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_outcome-ar_stdprice_total.sbatch
+sbatch --job-name=cv_alg-${1}_outcome-ar_stdprice_total_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_outcome-ar_stdprice_total.sbatch
 echo
