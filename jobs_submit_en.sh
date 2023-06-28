8d1c98ab53fa8d5d4cdb1b4d717250a3d85cd3cd
Felix Pichardo <fpichard@umn.edu>
2023-06-28 12:59:44 -0500

    Update to handle NaNs in outcome data

--- jobs_submit_en.sh
+++ jobs_submit_en.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# 1: algorithm: en, lasso, en
+# 1: algorithm: en, lasso, rfr
 # 2: pixel resolution: small, mid, large
 # 3: random seed
+# 4: outcome (a column from one of the outcome datasets)
 
 
 case ${1} in
@@ -25,53 +26,53 @@ case ${1} in
 esac
 
 echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_1_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_1.sbatch
-sbatch --job-name=cv_alg-${1}_1_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_1.sbatch
+echo sbatch --job-name=cv_alg-${1}_1_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_1.sbatch
+sbatch --job-name=cv_alg-${1}_1_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_1.sbatch
 echo 
 
 echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_1_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_1.sbatch
-sbatch --job-name=cv_alg-${1}_1_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_1.sbatch
+echo sbatch --job-name=cv_alg-${1}_1_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_1.sbatch
+sbatch --job-name=cv_alg-${1}_1_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_1.sbatch
 echo
 
 echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_2_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_2.sbatch
-sbatch --job-name=cv_alg-${1}_2_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_2.sbatch
+echo sbatch --job-name=cv_alg-${1}_2_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_2.sbatch
+sbatch --job-name=cv_alg-${1}_2_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_2.sbatch
 echo 
 
 echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_2_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_2.sbatch
-sbatch --job-name=cv_alg-${1}_2_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_2.sbatch
+echo sbatch --job-name=cv_alg-${1}_2_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_2.sbatch
+sbatch --job-name=cv_alg-${1}_2_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_2.sbatch
 echo
 
 echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_3_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_3.sbatch
-sbatch --job-name=cv_alg-${1}_3_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_3.sbatch
+echo sbatch --job-name=cv_alg-${1}_3_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_3.sbatch
+sbatch --job-name=cv_alg-${1}_3_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_3.sbatch
 echo 
 
 echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_3_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_3.sbatch
-sbatch --job-name=cv_alg-${1}_3_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_3.sbatch
-echo
-
-
-echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_4_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_4.sbatch
-sbatch --job-name=cv_alg-${1}_4_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_4.sbatch
-echo 
-
-echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_4_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_4.sbatch
-sbatch --job-name=cv_alg-${1}_4_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_4.sbatch
+echo sbatch --job-name=cv_alg-${1}_3_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_3.sbatch
+sbatch --job-name=cv_alg-${1}_3_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_3.sbatch
 echo
 
 
 echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_5_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_5.sbatch
-sbatch --job-name=cv_alg-${1}_5_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_5.sbatch
+echo sbatch --job-name=cv_alg-${1}_4_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_4.sbatch
+sbatch --job-name=cv_alg-${1}_4_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_4.sbatch
 echo 
 
 echo Submitting job...
-echo sbatch --job-name=cv_alg-${1}_5_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_5.sbatch
-sbatch --job-name=cv_alg-${1}_5_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_5.sbatch
+echo sbatch --job-name=cv_alg-${1}_4_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_4.sbatch
+sbatch --job-name=cv_alg-${1}_4_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_4.sbatch
+echo
+
+
+echo Submitting job...
+echo sbatch --job-name=cv_alg-${1}_5_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_5.sbatch
+sbatch --job-name=cv_alg-${1}_5_outcome-${4}_res-${2}_hdim-0_seed-${3} --export=alg=${1},resolution=${2},hdim=0,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_5.sbatch
+echo 
+
+echo Submitting job...
+echo sbatch --job-name=cv_alg-${1}_5_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_5.sbatch
+sbatch --job-name=cv_alg-${1}_5_outcome-${4}_res-${2}_hdim-1_seed-${3} --export=alg=${1},resolution=${2},hdim=1,seed=${3},outcome=${4} --time=${time} --mail-user=fpichard@umn.edu submit_scripts/cv_en_5.sbatch
 echo
